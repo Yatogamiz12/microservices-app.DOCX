5138f67bda5a3a7a149230adebd85ab16b15ab32
Yatogamiz12 <julianandresfonsecadiaz@gmail.com>
2026-09-13 15:52:16 -0500

    Add MongoDB connection function using mongoose

--- shared/db.js
+++ shared/db.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
 
+const connectDB = async (mongoUri) => {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB conectado exitosamente");
+  } catch (error) {
+    console.error("Error al conectar con MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
